ca31348d5ba6c95f2856b3bc550c778c7397d512
Student 6 <Student 6@10.8.33.173>
2017-01-10 12:35:05 -0500

    dfwe

---
+++ Daniel Introduction/src/gui/simon/ButtonInterfaceDaniel.java
@@ -0,0 +1,22 @@
+package gui.simon;
+
+import gui.components.Action;
+import gui.components.Clickable;
+import java.awt.Color;
+
+public interface ButtonInterfaceDaniel {
+
+	void setAction(Action a);
+
+	void highlight();
+
+	void setColor(Color color);
+
+	void dim();
+
+	void setX(int i);
+
+	void setY(int i);
+
+	//hello
+}
