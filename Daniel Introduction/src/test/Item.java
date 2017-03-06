e93407dbcfe63c5ff5cb0423f6e2009c64e4a077
Student 6 <Student 6@10.8.33.173>
2017-03-06 12:52:54 -0500

    The Just Fate of The Unworthy

--- Daniel Introduction/src/test/Item.java
+++ Daniel Introduction/src/test/Item.java
@@ -1,5 +1,15 @@
 package test;
 
 public class Item {
+	
+	private String description;
+	
+	private boolean purchased;
+	
+	public Item(String d){
+		this.description = d;
+		this.purchased = false;
+		
+	}
 
 }
