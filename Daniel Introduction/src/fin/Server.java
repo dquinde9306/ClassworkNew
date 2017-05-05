a43b40bf176d2b116bf9710fd7360e4bfa740908
Student 6 <Student 6@10.8.33.173>
2017-05-05 12:29:32 -0400

    May 5th update

--- Daniel Introduction/src/fin/Server.java
+++ Daniel Introduction/src/fin/Server.java
@@ -7,6 +7,26 @@ import java.awt.event.*;
 import javax.swing.*; 
 
 
-public class Server {
+public class Server extends JFrame {
+	
+	private JTextField userText;
+	private JTextArea chatWidow;
+	private ObjectOutputStream output; 
+	private ObjectInputStream input;	
+	private ServerSocket server;
+	private Socket connection;
 
+	public Server(){
+		super("Apollo");
+		userText = new JTextField();
+		userText.setEditable(false);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
 }
