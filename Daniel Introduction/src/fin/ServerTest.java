f84067943e75f96d4ffd2ef80d8b9bbb2d9f8267
Student 6 <Student 6@10.8.33.173>
2017-05-10 12:44:55 -0400

    Test for server created

---
+++ Daniel Introduction/src/fin/ServerTest.java
@@ -0,0 +1,13 @@
+package fin;
+
+import javax.swing.JFrame; 
+
+public class ServerTest {
+
+	public static void main(String[] args) {
+		Server ringo = new Server();
+		ringo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ringo.startRunning();
+	}
+
+}
