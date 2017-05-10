b01cbc445ed1772ef5cf1d8fb210f57ea9689df1
Student 6 <Student 6@10.8.33.173>
2017-05-10 12:57:03 -0400

    added imports

--- Daniel Introduction/src/fin/Client.java
+++ Daniel Introduction/src/fin/Client.java
@@ -1,5 +1,13 @@
 package fin;
 
-public class Client {
+import java.io.*;
+import java.net.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Client extends JFrame {
+	private JTextField userText;
+	private JTextArea chatWindow;
 
 }
