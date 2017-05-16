58fc30aee2bba37c347d6f3695c083b34c87f3c1
Student 6 <Student 6@10.8.33.173>
2017-05-16 12:35:56 -0400

    client test

--- Daniel Introduction/src/fin/ClientTest.java
+++ Daniel Introduction/src/fin/ClientTest.java
@@ -1,9 +1,13 @@
 package fin;
+import javax.swing.JFrame;
 
 public class ClientTest {
 
 	public static void main(String[] args) {
-
+		Client john;
+		john = new Client("165.155.196.87");
+		john.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		john.startRunning();
 	}
 
 }
