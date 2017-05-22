331aa39afaf8ed567ba6f23eb50817b0f56b9704
Student6 <Student6@10.8.33.161>
2017-05-22 13:08:42 -0400

    5 22 17

--- Daniel Introduction/src/fin/Login.java
+++ Daniel Introduction/src/fin/Login.java
@@ -1,5 +1,46 @@
 package fin;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
 public class Login {
+	
+	
+	static Scanner input = new Scanner(System.in);
+	
+	public static String waitForEntry(){
+		return input.nextLine();
+	}
+	
+	private static ArrayList<String> usernames = new ArrayList<String>();
+	
+	public static void main(String[] args){
+		window();
+		
+		usernames.add("a");
+		System.out.println("user?");
+		String entry = waitForEntry();
+		for (int i = 0; i < usernames.size(); i++){
+			if(entry.equals(usernames.get(i))){
+				System.out.println("!");
+			}
+			else{
+				System.out.println("?");
+			}
+		}
+	}
+	
+	public static void window(){
+		JFrame frame = new JFrame("This is my frame");
+		frame.setSize(300,400);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	
+	public static void buttons(){
+		
+	}
 
 }
