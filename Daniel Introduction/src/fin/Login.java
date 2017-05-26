d1cdb8ead6ad6e3951f6c86646dc74dae28bf0bc
Student6 <Student6@10.8.33.161>
2017-05-26 13:14:39 -0400

    5 26 17

--- Daniel Introduction/src/fin/Login.java
+++ Daniel Introduction/src/fin/Login.java
@@ -1,78 +1,81 @@
 package fin;
 
-import java.awt.BorderLayout;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.Scanner;
-
 import javax.swing.AbstractButton;
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.WindowConstants;
+import javax.swing.JFrame;
 
-public class Login extends JPanel
-//implements ActionListener
-{
-	
-	
-	static Scanner input = new Scanner(System.in);
-	
-	public static String waitForEntry(){
-		return input.nextLine();
-	}
-	
-	private static ArrayList<String> usernames = new ArrayList<String>();
-	private static JButton b1;
-	private static JButton b2;
-	
-	public static void main(String[] args){
-		window();
-		buttons();
-		usernames.add("a");
-		System.out.println("user?");
-		String entry = waitForEntry();
-		for (int i = 0; i < usernames.size(); i++){
-			if(entry.equals(usernames.get(i))){
-				System.out.println("!");
-			}
-			else{
-				System.out.println("?");
-			}
-		}
-	}
-	
-	public static void window(){
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+public class Login extends JPanel implements ActionListener {
+	protected static JButton b1;
+	protected static JButton b2;
+
+	public static void main(String[] args) {
+		//Schedule a job for the event-dispatching thread:
+		//creating and showing this application's GUI.
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				createAndShowGUI(); 
-			}
+		public void run() {
+		createAndShowGUI(); 
+		}
 		});
 	}
-
 	
-
+	/**
+	* Create the GUI and show it.  For thread safety, 
+	* this method should be invoked from the 
+	* event-dispatching thread.
+	*/
 	private static void createAndShowGUI() {
-
+	
 		//Create and set up the window.
 		JFrame frame = new JFrame("Register or Login?");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		ButtonDemo newContentPane = new ButtonDemo();
+	
+		//Create and set up the content pane.
+		Login newContentPane = new Login();
 		newContentPane.setOpaque(true); //content panes must be opaque
 		frame.setContentPane(newContentPane);
 	
+	
 		//Display the window.
-		frame.setSize(300,400);
+		frame.setSize(800,800);
 		frame.setVisible(true);
-//		frame.add(b1, BorderLayout.PAGE_START); 
+	
+	
 	}
-
-	public static void buttons(){ 
+	
+	public Login() {
+	
 		b1 = new JButton("Register");
-		b2 = new JButton("Login");
-		//b1.setLayout(null);
+		b1.setActionCommand("disable");
 		
+		b2 = new JButton("Login");
+		b2.setActionCommand("disable");
+		
+		
+		//Listen for actions on buttons 1 and 3.
+		b1.addActionListener(this);
+		b2.addActionListener(this);
+		
+		//Add Components to this container, using the default FlowLayout.
+		add(b1);
+		add(b2);
+	
+	
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if ("disable".equals(e.getActionCommand())) {
+			b2.setEnabled(false);
+			b1.setEnabled(false);
+		} else {
+			b2.setEnabled(true);
+			b1.setEnabled(true);
+		}
 	}
 
 }
