c37ced283bdee8c0d23be625e65c876c6656f7b4
Student6 <Student6@10.8.33.161>
2017-06-07 12:48:38 -0400

    6 7 17

---
+++ Daniel Introduction/src/fin/NextPage.java
@@ -0,0 +1,15 @@
+package fin;
+
+import javax.swing.*;
+import java.awt.*;
+ 
+class NextPage extends JFrame
+{
+  NextPage()
+ {
+ setDefaultCloseOperation(javax.swing.
+  WindowConstants.DISPOSE_ON_CLOSE);
+ setTitle("Welcome");
+ setSize(500, 500);
+  }
+ }
